0b84e532e1869bed03fbcaf58704f3da48758951
qj p <qj p>
2018-02-26 23:19:04 +0800

    2018/02/26 23:18:44

--- Partition Array/test.cpp
+++ Partition Array/test.cpp
@@ -10,34 +10,26 @@ public:
      * @param k: An integer
      * @return: The index after partition
      */
-    int partitionArray(vector<int> &nums, int k, int s, int e) {
+    int partitionArray(vector<int> &nums, int k) {
         // write your code here
-        int j = s-1;
-        int p = e;//-1;
-        for (int i=s; i < e; i++) {
+        int j = -1;
+        int p = -1;
+        for (int i=0; i < nums.size(); i++) {
             if (nums[i] < k)
                 swap(nums[++j], nums[i]);
-            //else if (nums[i] == k && p == -1)
-             //   p = i;
+            else if (nums[i] == k && p == -1)
+                p = i;
         }
-        //if (p != -1)
+        if (p != -1)
             swap(nums[j+1], nums[p]);
         return j + 1;
-    }
-    
-    void sort(vector<int> &arr, int s, int e) {
-        if (s >= e) return;
-        
-        int p = partitionArray(arr, arr[e], s, e);
-        
-        sort(arr, s, p-1);
-        sort(arr, p+1, e);
     }
 };
 
 int main() {
-    vector<int> arr = {9,9,9,8,9,8,7,9,8,8,8,9,8,9,8,8,6,9};
-    Solution().sort(arr, 0, arr.size()-1) ;
-    for (int x : arr)
-        cout << x << " " ;
+    vector<int> nums = {2, 3, 4, 1, 4, 6, 8};
+    cout << Solution().partitionArray(nums, 8) << endl;
+    for (int n : nums)
+        cout << n << " ";
+    cout << endl;
 }
