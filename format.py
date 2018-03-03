2439b4b23c34beafa4141984c04f39ba070309a6
qj p <qj p>
2018-03-03 19:44:14 +0800

    code format

--- format.py
+++ format.py
@@ -1,6 +1,13 @@
 import os
 
-for path, _, files in os.walk("."):
+for path, dirs, files in os.walk("."):
+    if path == "." or path == "./":
+        for dir in dirs:
+            new_name = [word for word in dir.replace("-", " ").split(" ") if word.strip()]
+            new_name = " ".join([word[0].upper()+word[1:] for word in new_name])
+            if new_name != dir:
+                os.rename(dir, new_name)
+                print ("rename dir %s to %s" % (dir, new_name))
     for fname in files:
         if fname.endswith(".py") or fname.endswith(".cpp") or fname.endswith(".c") or fname.endswith(".h"):
             f = os.path.join(path, fname)
